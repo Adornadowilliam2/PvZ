08cfcad776ea59837bea334e15cd6fa0d3346563
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-09-27 20:39:08 +0800

    add a search function to see the data

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,69 @@ import { useState } from "react";
 
 import "./App.css";
 import logo from "./assets/logo.png";
-import sunflower from "./assets/sunflower.jpeg";
-import wallnut from "./assets/wallnut.jpeg";
-import cherrybomb from "./assets/cherrybomb.jpeg";
-import potatomine from "./assets/potatomine.jpeg";
-import peashooter from "./assets/peashooter.jpeg";
-import zombie from "./assets/zombie.jpeg";
-import muzan from "./assets/michael.jpeg";
+import sunflower from "./assets/sunflower.png";
+import wallnut from "./assets/wallnut.png";
+import cherrybomb from "./assets/cherrybomb.png";
+import potatomine from "./assets/potatomine.png";
+import peashooter from "./assets/peashooter.png";
+import zombie from "./assets/zombie.png";
+import muzan from "./assets/michael.png";
 import giant from "./assets/giant.png";
+
+const data = [
+  {
+    id: 1,
+    name: "sunflower",
+    image: sunflower,
+  },
+  {
+    id: 2,
+    name: "peashooter",
+    image: peashooter,
+  },
+  {
+    id: 3,
+    name: "potatomine",
+    image: potatomine,
+  },
+  {
+    id: 4,
+    name: "wallnut",
+    image: wallnut,
+  },
+  {
+    id: 5,
+    name: "cherrybomb",
+    image: cherrybomb,
+  },
+
+  {
+    id: 6,
+    name: "zombie",
+    image: zombie,
+  },
+  {
+    id: 7,
+    name: "muzan",
+    image: muzan,
+  },
+  {
+    id: 8,
+    name: "giant",
+    image: giant,
+  },
+];
+
 function App() {
+  const [search, setSearch] = useState("");
+  const filteredData = data.filter((item) =>
+    item.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <>
       <nav className="bg-dark text-white p-5">
@@ -22,45 +76,46 @@ function App() {
       </nav>
       <main>
         <img src={logo} alt="logo" className="logo" />
+        <div className="search-container">
+          <input
+            type="text"
+            name="input_search"
+            id="input_search"
+            value={search}
+            onChange={handleSearch}
+            placeholder="Search..."
+          />
+          <button>Search</button>
+        </div>
       </main>
-      <h2>Plants</h2>
-      <fieldset>
-        <div className="card">
-          <img src={sunflower} alt="sunflower" />
-          <h2>Sunflower</h2>
-        </div>
-        <div className="card">
-          <img src={peashooter} alt="peashooter" />
-          <h2>Pea Shooter</h2>
-        </div>
-        <div className="card">
-          <img src={potatomine} alt="potatomine" />
-          <h2>Potato Mine</h2>
-        </div>
-        <div className="card">
-          <img src={wallnut} alt="wallnut" />
-          <h2>Wallnut</h2>
-        </div>
-        <div className="card">
-          <img src={cherrybomb} alt="cherry bomb" />
-          <h2>Cherry Bomb</h2>
-        </div>
-      </fieldset>
-      <h2>Zombies</h2>
-      <fieldset>
-        <div className="card">
-          <img src={zombie} alt="zombie" />
-          <h2>Zombie</h2>
-        </div>
-        <div className="card">
-          <img src={muzan} alt="muzan" />
-          <h2>Muzan</h2>
-        </div>
-        <div className="card">
-          <img src={giant} alt="giant" />
-          <h2>Giant</h2>
-        </div>
-      </fieldset>
+
+      <div
+        className="container"
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          flexWrap: "wrap",
+          alignItems: "center",
+          gap: "10px",
+        }}
+      >
+        {filteredData.map((item) => (
+          <div
+            style={{
+              textAlign: "center",
+              border: "1px solid black",
+              padding: "10px",
+            }}
+          >
+            <img
+              src={item.image}
+              alt={item.name}
+              style={{ width: "200px", background: "white" }}
+            />
+            <h1>{item.name}</h1>
+          </div>
+        ))}
+      </div>
     </>
   );
 }

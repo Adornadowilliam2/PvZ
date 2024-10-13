4f16498cb06100015e97274bb61f20660f1e3afe
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-10-13 12:40:00 +0800

    changing the design:

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 import logo from "./assets/logo.png";
-import sunflower from "./assets/sunflower.png";
-import wallnut from "./assets/wallnut.png";
-import cherrybomb from "./assets/cherrybomb.png";
-import potatomine from "./assets/potatomine.png";
-import peashooter from "./assets/peashooter.png";
-import zombie from "./assets/zombie.png";
-import muzan from "./assets/michael.png";
-import giant from "./assets/giant.png";
-
-const data = [
-  {
-    id: 1,
-    name: "sunflower",
-    image: sunflower,
-  },
-  {
-    id: 2,
-    name: "peashooter",
-    image: peashooter,
-  },
-  {
-    id: 3,
-    name: "potatomine",
-    image: potatomine,
-  },
-  {
-    id: 4,
-    name: "wallnut",
-    image: wallnut,
-  },
-  {
-    id: 5,
-    name: "cherrybomb",
-    image: cherrybomb,
-  },
-
-  {
-    id: 6,
-    name: "zombie",
-    image: zombie,
-  },
-  {
-    id: 7,
-    name: "muzan",
-    image: muzan,
-  },
-  {
-    id: 8,
-    name: "giant",
-    image: giant,
-  },
-];
+import { data } from "./index.js";
 
 function App() {
   const [search, setSearch] = useState("");
-  const filteredData = data.filter((item) =>
-    item.name.toLowerCase().includes(search.toLowerCase())
-  );
+  const allData = data.flat();
+  const [filter, setFilter] = useState([]);
 
-  const handleSearch = (e) => {
-    setSearch(e.target.value);
-  };
+  useEffect(() => {
+    const filteredData = allData.filter((item) =>
+      item.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilter(filteredData);
+  }, [search]);
 
   return (
     <>
-      <nav className="bg-dark text-white p-5">
+      <nav className="bg-dark text-white p-2 position-fixed w-100 z-2">
         <div className="container-fluid d-flex justify-content-between align-items-center">
           <div className="navbar-header">
             <a className="navbar-brand">Plant Vs Zombie</a>
@@ -75,17 +26,17 @@ function App() {
         </div>
       </nav>
       <main>
-        <img src={logo} alt="logo" className="logo" />
+        <div className="logo"></div>
         <div className="search-container">
+          <h1>Search</h1>
           <input
             type="text"
             name="input_search"
             id="input_search"
             value={search}
-            onChange={handleSearch}
+            onChange={(e) => setSearch(e.target.value)}
             placeholder="Search..."
           />
-          <button>Search</button>
         </div>
       </main>
 
@@ -99,19 +50,21 @@ function App() {
           gap: "10px",
         }}
       >
-        {filteredData.map((item) => (
-          <div
-            style={{
-              textAlign: "center",
-              border: "1px solid black",
-              padding: "10px",
-            }}
-          >
-            <img
-              src={item.image}
-              alt={item.name}
-              style={{ width: "200px", background: "white" }}
-            />
+        {filter.map((item) => (
+          <div className="card" key={item.gameId}>
+            <span
+              style={{
+                position: "absolute",
+                top: "10px",
+                left: "10px",
+                zIndex: "1",
+                background: "white",
+                padding: "10px",
+                borderRadius: "50%",
+                width: "50px",
+              }}
+            >{`${item.gameId}`}</span>
+            <img src={item.image} alt={item.name} />
             <h1>{item.name}</h1>
           </div>
         ))}
---
+++ src/index.js
@@ -0,0 +1,90 @@
+import sunflower from "./assets/sunflower.png";
+import wallnut from "./assets/wallnut.png";
+import cherrybomb from "./assets/cherrybomb.png";
+import potatomine from "./assets/potatomine.png";
+import peashooter from "./assets/peashooter.png";
+import zombie from "./assets/zombie.png";
+import muzan from "./assets/michael.png";
+import giant from "./assets/giant.png";
+import snowpea from "./assets/snowpea.png";
+import garlic from "./assets/garlic.png";
+import chili from "./assets/chili.png";
+import squash from "./assets/squash.png";
+export const data = [
+  [
+    {
+      id: 1,
+      gameId: 1,
+      name: "sunflower",
+      image: sunflower,
+    },
+    {
+      id: 2,
+      gameId: 2,
+      name: "peashooter",
+      image: peashooter,
+    },
+    {
+      id: 3,
+      gameId: 3,
+      name: "potatomine",
+      image: potatomine,
+    },
+    {
+      id: 4,
+      gameId: 4,
+      name: "wallnut",
+      image: wallnut,
+    },
+    {
+      id: 5,
+      gameId: 5,
+      name: "cherrybomb",
+      image: cherrybomb,
+    },
+    {
+      id: 6,
+      gameId: 6,
+      name: "snow pea",
+      image: snowpea,
+    },
+    {
+      id: 7,
+      gameId: 7,
+      name: "garlic",
+      image: garlic,
+    },
+    {
+      id: 8,
+      gameId: 8,
+      name: "chili",
+      image: chili,
+    },
+    {
+      id: 9,
+      gameId: 9,
+      name: "squash",
+      image: squash,
+    },
+  ],
+  [
+    {
+      id: 1,
+      gameId: 10,
+      name: "zombie",
+      image: zombie,
+    },
+    {
+      id: 2,
+      gameId: 11,
+      name: "muzan",
+      image: muzan,
+    },
+    {
+      id: 3,
+      gameId: 12,
+      name: "giant",
+      image: giant,
+    },
+  ],
+];

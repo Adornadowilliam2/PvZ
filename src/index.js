e01bda179a906bf6f49287c9ab7558fc6a9cbaff
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-10-15 11:09:18 +0800

    adding some fucking mushroom

--- src/index.js
+++ src/index.js
@@ -14,73 +14,123 @@ import corncatapult from "./assets/corncatapult.png";
 import cabbagecatapult from "./assets/cabbagecatapult.png";
 import tallnut from "./assets/tallnut.png";
 import spikeweed from "./assets/spikeweed.png";
+import puffshroom from "./assets/puffshroom.png";
+import sunshroom from "./assets/sunshroom.png";
+import hypnoshroom from "./assets/hypnoshroom.png";
+import iceshroom from "./assets/iceshroom.png";
+import doomshroom from "./assets/doomshroom.png";
+const field = {
+  day: "day",
+  night: "night",
+};
 export const data = [
   [
     {
       name: "sunflower",
       image: sunflower,
+      field: field.day,
     },
     {
       name: "peashooter",
       image: peashooter,
+      field: field.day,
     },
     {
       name: "potatomine",
       image: potatomine,
+      field: field.day,
     },
     {
       name: "wallnut",
       image: wallnut,
+      field: field.day,
     },
     {
       name: "cherrybomb",
       image: cherrybomb,
+      field: field.day,
     },
     {
       name: "snow pea",
       image: snowpea,
+      field: field.day,
     },
     {
       name: "garlic",
       image: garlic,
+      field: field.day,
     },
     {
       name: "chili",
       image: chili,
+      field: field.day,
     },
     {
       name: "squash",
       image: squash,
+      field: field.day,
     },
     {
       name: "spikeweed",
       image: spikeweed,
+      field: field.day,
     },
     {
       name: "cabbage catapult",
       image: cabbagecatapult,
+      field: field.day,
     },
     {
       name: "corn catapult",
       image: corncatapult,
+      field: field.day,
     },
     {
       name: "tallnut",
       image: tallnut,
+      field: field.day,
+    },
+    {
+      name: "puff shroom",
+      image: puffshroom,
+      field: field.night,
+    },
+    {
+      name: "sun shroom",
+      image: sunshroom,
+      field: field.night,
+    },
+    {
+      name: "doom shroom",
+      image: doomshroom,
+      field: field.night,
+    },
+    {
+      name: "ice shroom",
+      image: iceshroom,
+      field: field.night,
+    },
+    {
+      name: "hypno shroom",
+      image: hypnoshroom,
+      field: field.night,
     },
   ],
   [
     {
       name: "zombie",
       image: zombie,
+      field: field.day,
     },
     {
       name: "muzan",
       image: muzan,
+      field: field.day,
     },
     {
       name: "giant",
       image: giant,
+      field: field.day,
     },
   ],
 ];
--- src/sample.js
+++ src/sample.js
@@ -1,22 +1,36 @@
+const field = {
+  land: "land",
+  water: "water",
+  galaxy: "galaxy",
+};
 const arr = [
   [
     {
       id: 1,
       name: "Goku",
+      live: field.land,
     },
     {
       id: 2,
       name: "Vegita",
+      live: field.galaxy,
     },
   ],
   [
     {
       id: 1,
       name: "Freiza",
+      live: field.galaxy,
     },
     {
       id: 2,
       name: "Raditz",
+      live: field.galaxy,
+    },
+    {
+      id: 3,
+      name: "Fish",
+      live: field.water,
     },
   ],
 ];
